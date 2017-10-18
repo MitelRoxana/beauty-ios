c0a955c7161b9209c995c6bc32a7b0cd418f0e21
Roxana Mitel <roxana.mitel@codegile.com>
2017-10-18 11:40:34 +0300

    Initial Commit

---
+++ Beauty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beauty
+//
+//  Created by Roxana Mitel on 18/10/2017.
+//  Copyright © 2017 roxanamitel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
